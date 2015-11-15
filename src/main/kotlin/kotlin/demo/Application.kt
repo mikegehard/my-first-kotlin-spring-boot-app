75bf6e84cc3c9cff1ff32483344ac75cff1b5fae
Mike Gehard <mike.gehard@gmail.com>
2015-11-14 17:14:59 -0700

    My first kotlin/Spring Boot application

---
+++ src/main/kotlin/kotlin/demo/Application.kt
@@ -0,0 +1,20 @@
+package kotlin.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+//@SpringBootApplication
+//open class Application
+//
+//fun main(args: Array<String>) {
+//    SpringApplication.run(Application::class.java, *args)
+//}
+
+@SpringBootApplication
+open class Application {
+    companion object {
+        @JvmStatic public fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
